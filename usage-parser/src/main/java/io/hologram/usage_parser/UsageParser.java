a3746b2373a69d154f605558df324cd0f6cce890
Nathan Ginther <nathan@hologram.io>
2022-08-10 15:24:23 -0400

    Initial commit

---
+++ usage-parser/src/main/java/io/hologram/usage_parser/UsageParser.java
@@ -0,0 +1,15 @@
+package io.hologram.usage_parser;
+
+import java.util.List;
+import io.hologram.usage_parser.objects.Usage;
+
+/**
+ * Starting parser class
+ */
+public class UsageParser
+{
+    public List<Usage> parse(List<String> usage){
+        // Add your code here!
+    }
+
+}
